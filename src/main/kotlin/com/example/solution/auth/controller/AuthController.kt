42c76be8bc65b25f4ed2c01e950b93da0a269e35
npureaun <98468118+npureaun@users.noreply.github.com>
2025-02-10 17:53:23 +0900

    ♻️ authService dependency

--- src/main/kotlin/com/example/solution/auth/controller/AuthController.kt
+++ src/main/kotlin/com/example/solution/auth/controller/AuthController.kt
@@ -2,6 +2,7 @@ package com.example.solution.auth.controller
 
 import com.example.solution.auth.dto.UserVerifyCheckRequestDto
 import com.example.solution.auth.dto.UserVerifyCodeRequestDto
+import com.example.solution.auth.service.AuthService
 import com.example.solution.util.twilio.TwilioComponent
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -11,16 +12,16 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class AuthController(
-    private val twilioComponent: TwilioComponent
+    private val authService: AuthService
 ) {
     @PostMapping("/user/auth/sms/code")
     fun getUserAuthenticateCode(@RequestBody userVerifyCodeRequestDto: UserVerifyCodeRequestDto): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.OK).body(twilioComponent.verification(userVerifyCodeRequestDto))
+        return ResponseEntity.status(HttpStatus.OK).body(authService.twilioVerificationSend(userVerifyCodeRequestDto))
     }
 
     @PostMapping("/user/auth/sms/check")
     fun verifyUserAuthenticateCode(@RequestBody userVerifyCheckRequestDto: UserVerifyCheckRequestDto)
     : ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.OK).body(twilioComponent.verificationCheck(userVerifyCheckRequestDto))
+        return ResponseEntity.status(HttpStatus.OK).body(authService.twilioVerificationCheck(userVerifyCheckRequestDto))
     }
 }
